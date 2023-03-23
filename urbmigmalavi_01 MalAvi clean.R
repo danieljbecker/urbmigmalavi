e91b4c045bd347079ec8bf85beb07da68c5302d5
Daniel Becker <10813169+danieljbecker@users.noreply.github.com>
2023-03-23 14:25:20 -0500

    starting to rescript

---
+++ urbmigmalavi_01 MalAvi clean.R
@@ -0,0 +1,12 @@
+## MalAvi, urbanization, migration
+## 01_MalAvi clean
+## danbeck@ou.edu
+## last updated 3/23/2023
+
+## clean environment & plots
+rm(list=ls()) 
+graphics.off()
+gc()
+
+## libraries
+library(malaviR)
