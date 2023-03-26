71c1a2e6070daf535631fefb70e6273fa469cf46
Daniel Becker <dbecker@uga.edu>
2023-03-26 14:44:39 -0500

    reorganizing phylos

---
+++ urbmigmalavi_03 cophylo.R
@@ -0,0 +1,13 @@
+## MalAvi, urbanization, migration
+## 03_cophylo
+## danbeck@ou.edu
+## last updated 3/26/2023
+
+## clean environment & plots
+rm(list=ls()) 
+graphics.off()
+gc()
+
+## libraries
+library(ape)
+library(caper)
